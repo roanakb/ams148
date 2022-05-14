28f4ef20b63fa25c3f4ab6da378cf1941d3384e8
Roanak Baviskar <roanakbaviskar@gmail.com>
2022-05-13 21:28:34 -0700

    added shared memory parallel matrix transpose

--- hip/transpose/transpose.cpp
+++ hip/transpose/transpose.cpp
@@ -23,6 +23,111 @@ __global__ void TransposeKernel(const Matrix, Matrix);
 __global__ void naivekernel(const Matrix, Matrix); 
 
 
+/* Shared Matrix Multiplication Routines */ 
+
+/* Transpose with shared memory 
+   :inputs: Matrix A
+   :outputs: Matrix B = A^T
+ */ 
+void Transpose(const Matrix A, Matrix B)
+{
+    int Gpu = 1; 
+    int toDev = 1, fromDev = 2; 
+    //Load A and B to device memory 
+    //Allocate Matrix C
+    Matrix d_A(A.width, A.height, A.stride, Gpu);
+    Matrix d_B(B.width, B.height, B.stride, Gpu);
+    d_A.load(A, toDev);
+	
+    // Invoke Kernel
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 dimGrid(A.width / dimBlock.x, A.height/ dimBlock.y); 
+    //Use HIP Events for timing
+    hipEvent_t start, stop; 
+    float time; 
+    hipEventCreate(&start); 
+    hipEventCreate(&stop); 
+    hipEventRecord(start, 0); 
+
+    TransposeKernel<<<dimGrid, dimBlock>>>(d_A, d_B);
+    hipEventRecord(stop, 0); 
+    hipEventSynchronize(stop); 
+    hipEventElapsedTime(&time, start, stop); 
+    std::cout<< " Shared Memory Matrix Multiplication time =" << '\t' 
+             << time << "ms" << std::endl; 
+
+	// Read B from Device memory 
+    B.load(d_B, fromDev);
+	
+    //Free device memory 
+    d_A.dealloc(Gpu);
+    d_B.dealloc(Gpu);
+}
+
+
+
+// Matrix TransposeKernel
+__global__ void TranposeKernel(Matrix A, Matrix B)
+{
+	//Static shared memory for Asub and Bsub
+	__shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+
+
+	// Block row and column;
+	int blockRow = blockIdx.y;
+	int blockCol = blockIdx.x;
+
+   int brow = 0;
+   int bcol = 0;
+
+   // IF BLOCK ROW == BLOCK COL, we can get same sub matrix of C
+   // IF NOT, we need to place items in transposed location
+   if (blockRow == blockCol) {
+      brow = blockRow;
+      bcol = blockCol;
+   }
+   else {
+      brow = blockCol;
+      bcol = blockRow;
+   }
+	//Thread block computes one sub matrix Csub of C
+	subMatrix Csub(C, BLOCK_SIZE,  brow, bcol);
+
+	// Each thread computes one element of Csub
+	// By accumulating results into Cvalue
+	float Cvalue = 0.0f; 
+
+	//Thread row and column index within the submatrix
+	int row = threadIdx.y;
+	int col = threadIdx.x; 
+
+	// Loop over submatrices of A and B that are required for Csub
+	//Multiply each pair of sub-matrices together
+	//and summ the results
+	for (int m = 0; m < (A.width/BLOCK_SIZE); m++){
+		
+		//Get A submatrix
+		subMatrix Asub(A, BLOCK_SIZE, blockRow, m);
+
+		//Load Asub and Bsub from global memory into shared; 
+
+		As[row][col] = Asub.GetElem(row,col);
+
+		//Always sync threads when loading shared memory before doing computation
+		__syncthreads();
+
+		//Set Cvalue to correct value
+      Cvalue = As[row][col]
+
+		//synchronize to make sure all threads are done computing
+		__syncthreads();
+	}
+	//write Csub back into global memory 
+	//each thread writes one element
+	Csub.SetElem(col, row, Cvalue);
+}
+
+
 __global__ void naivekernel(const Matrix A, Matrix B)
 {
 	// Each Thread computes one element of B
@@ -101,10 +206,10 @@ void time_transpose(int N)
 	double fullcpu = double(end - begin) / (CLOCKS_PER_SEC*12);
 	std::cout<< " CPU Time = " << fullcpu << "s" << std::endl; //*/
 //Naive CUDA
-	NaiveTranspose(A,B);
+	//NaiveTranspose(A,B);
 
 //SharedMemCUDA
-	//MatMul(A,B,C);
+	Transpose(A,B);
 	
 
 //Deallocate Memory
